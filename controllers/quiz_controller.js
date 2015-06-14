2cbffbda890f1028d218e3ccec5e18cf8243d07c
Natalio Ruiz <natalio.g.ruiz@gmail.com>
2015-06-14 09:39:10 -0300

    Edit & Delete functionality

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -40,5 +40,57 @@ exports.index = function(req, res) {
     } else {
         models.Quiz.findAll().then(render).catch(function(error) { next(error); });
     }
+};
+
+exports.new = function(req, res) {
+    var quiz = models.Quiz.build(
+        {
+            pregunta: "Pregunta",
+            respuesta: "Respuesta"
+        }
+    )
+
+    res.render('quizes/new', {quiz: quiz});
+}
+
+exports.create = function(req, res) {
+    var quiz = models.Quiz.build(req.body.quiz);
+
+    quiz.validate().then(function(err) {
+        if (err) {
+            res.render('quizes/new', {quiz: quiz, errors: err.errors });
+        } else {
+            quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+                res.redirect('/quizes');
+            });
+        }
+    });
 
 };
+
+exports.edit = function(req, res) {
+    var quiz =  req.quiz;
+
+    res.render('quizes/edit', { quiz: quiz });
+};
+
+exports.update = function(req, res) {
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+
+    req.quiz.validate().then(function(err) {
+        if (err) {
+            res.render('/quizes/edit', { quiz: req.quiz, errors: err.errors });
+        } else {
+            req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+                res.redirect('/quizes');
+            });
+        }
+    });
+};
+
+exports.destroy = function(req, res) {
+    req.quiz.destroy().then(function() {
+        res.redirect('/quizes');
+    }).catch(function(error) { next(error); });
+}
